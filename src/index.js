86ef3ea08e00a67b638072c3788b0cc28f1767d1
Giannis Mparous <p3190131@aueb.gr>
2024-04-19 15:36:40 +0300

    attempting to fix 404 github

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <HelmetProvider>
-      <HashRouter basename={process.env.PUBLIC_URL}>
+      <HashRouter path="/" basename={process.env.PUBLIC_URL}>
         <App />
       </HashRouter>
     </HelmetProvider>
