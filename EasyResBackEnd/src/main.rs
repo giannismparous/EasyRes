a1977d66c443adfc8e8094acc9555e4f47d0f0e9
Dimitrios Rammos <61520191+drammos@users.noreply.github.com>
2024-10-02 20:35:41 +0300

    Update the main

--- EasyResBackEnd/src/main.rs
+++ EasyResBackEnd/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::net::IpAddr;
+use std::net::SocketAddr;
+use once_cell::sync::Lazy;
+use EasyResBackEnd::{
+    error::{AppError, Result},
+    config::CONFIG
+};
+
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt::init();
+    let config = Lazy::force(&CONFIG)
+        .as_ref()
+        .map_err(|err| AppError::Startup(err.to_string()))?;
+
+    println!("Hello world {:?}", config);
+    Ok(())
 }
