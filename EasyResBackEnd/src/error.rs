0b69b2ba9bea5414e1883126ff7b8f0884412c2a
Dimitrios Rammos <61520191+drammos@users.noreply.github.com>
2024-10-02 20:35:17 +0300

    Update erro.rs

--- EasyResBackEnd/src/error.rs
+++ EasyResBackEnd/src/error.rs
@@ -2,11 +2,11 @@ use {
     axum::{
         http::StatusCode,
         response::{IntoResponse, Response},
+        Json,
     },
     serde_json::json,
 };
 
-// use crate::Json;
 pub type Result<T> = std::result::Result<T, AppError>;
 
 #[derive(thiserror::Error, Debug)]
@@ -14,8 +14,8 @@ pub enum AppError{
     #[error("Value not found")]
     NotFound,
     #[error("Configuration Error {0}")]
-    // Config(#[from] config::ConfigError),
-    // #[error("Could not start service: {0}")]
+    Config(#[from] config::ConfigError),
+    #[error("Could not start service: {0}")]
     Startup(String),
 
     #[error("Not found user with id: {0}")]
@@ -26,14 +26,14 @@ impl IntoResponse for AppError {
     fn into_response(self) -> Response {
         let (status, error_message) = match self {
             Self::NotFound => (StatusCode::NOT_FOUND, "Resource not Found"),
-            // Self::Config(_)
+            Self::Config(_)
             | Self::Startup(_)
             | Self::UserNotFound(_) => {
                 unreachable!("This error can only occur during startup")
             }
         };
 
-        let body = json!({"error": error_message});
+        let body = Json(json!({ "error": error_message }));
         (status, body).into_response()
     }
 }
