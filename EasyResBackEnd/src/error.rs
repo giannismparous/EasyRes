e779e6e95523dbbf90ffa6234a21497c718a0ac2
Dimitrios Rammos <61520191+drammos@users.noreply.github.com>
2024-10-02 13:53:46 +0300

    Update error.rs and add lib

--- EasyResBackEnd/src/error.rs
+++ EasyResBackEnd/src/error.rs
@@ -6,15 +6,16 @@ use {
     serde_json::json,
 };
 
-use crate::Json;
+// use crate::Json;
+pub type Result<T> = std::result::Result<T, AppError>;
 
-#[derive(this::Error, Debug)]
+#[derive(thiserror::Error, Debug)]
 pub enum AppError{
     #[error("Value not found")]
     NotFound,
     #[error("Configuration Error {0}")]
-    Config(#[from] config::ConfigError),
-    #[error("Could not start service: {0}")]
+    // Config(#[from] config::ConfigError),
+    // #[error("Could not start service: {0}")]
     Startup(String),
 
     #[error("Not found user with id: {0}")]
@@ -25,14 +26,14 @@ impl IntoResponse for AppError {
     fn into_response(self) -> Response {
         let (status, error_message) = match self {
             Self::NotFound => (StatusCode::NOT_FOUND, "Resource not Found"),
-            Self::Config(_)
+            // Self::Config(_)
             | Self::Startup(_)
             | Self::UserNotFound(_) => {
                 unreachable!("This error can only occur during startup")
             }
         };
 
-        let body = Json(json!({"error": error_message}));
+        let body = json!({"error": error_message});
         (status, body).into_response()
     }
 }
---
+++ EasyResBackEnd/src/lib.rs
@@ -0,0 +1,10 @@
+pub mod config;
+pub mod api;
+pub mod routes;
+pub mod services;
+// pub mod openapi;
+pub mod error;
+
+pub use crate::{
+    error::AppError
+};
