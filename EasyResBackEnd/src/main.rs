cdbcb4fb73eb4382a11ed6eb06bf1b353f98483d
Dimitrios Rammos <61520191+drammos@users.noreply.github.com>
2024-10-03 16:39:23 +0300

    Update main.rs with called the startup

--- EasyResBackEnd/src/main.rs
+++ EasyResBackEnd/src/main.rs
@@ -1,10 +1,15 @@
 use std::env;
-use std::net::IpAddr;
-use std::net::SocketAddr;
+use std::net::{
+    IpAddr, Ipv4Addr,
+    SocketAddr,
+    TcpListener,
+};
 use once_cell::sync::Lazy;
+
 use EasyResBackEnd::{
     error::{AppError, Result},
-    config::CONFIG
+    config::CONFIG,
+    updown::startup
 };
 
 
@@ -16,5 +21,15 @@ async fn main() -> Result<()> {
         .map_err(|err| AppError::Startup(err.to_string()))?;
 
     println!("Hello world {:?}", config);
+
+    /// SET THE ADRESS
+    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), config.port);
+    tracing::info!("Listening on {}", addr);
+    let listener = TcpListener::bind(&addr).or(Err(AppError::TcpBind))?;
+
+    startup::run(listener)?
+        .await
+        .map_err(|e| AppError::Startup(e.to_string()))?;
+
     Ok(())
 }
