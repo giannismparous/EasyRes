5f5585868d5b0370c26023ccc9bd8014ba125e69
Dimitrios Rammos <61520191+drammos@users.noreply.github.com>
2024-10-04 20:42:47 +0300

    Update the mod.rs with router

--- EasyResBackEnd/src/routes/mod.rs
+++ EasyResBackEnd/src/routes/mod.rs
@@ -1,1 +1,14 @@
 pub mod handler;
+mod user;
+mod restaurant;
+
+
+use axum::{Router};
+// use crate::routes::{handler, user, restaurant};
+
+pub fn routes() -> Router {
+    Router::new()
+        .merge(handler::routes())      // Κοινά endpoints
+        .nest("/api/user", user::routes())         // User-related endpoints
+        .nest("/api/restaurant", restaurant::routes()) // Restaurant-related endpoints
+}
