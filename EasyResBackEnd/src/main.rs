162151a7c2581ad779176518faa2735c75a64831
Dimitrios Rammos <61520191+drammos@users.noreply.github.com>
2024-10-08 15:51:16 +0300

    Corrections from cargo clippy

--- EasyResBackEnd/src/main.rs
+++ EasyResBackEnd/src/main.rs
@@ -1,17 +1,11 @@
-use std::env;
-use std::net::{
-    IpAddr, Ipv4Addr,
-    SocketAddr,
-    TcpListener,
-};
 use once_cell::sync::Lazy;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr, TcpListener};
 
 use EasyResBackEnd::{
-    error::{AppError, Result},
     config::CONFIG,
-    updown::startup
+    error::{AppError, Result},
+    updown::startup,
 };
-
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -22,10 +16,10 @@ async fn main() -> Result<()> {
 
     println!("Hello world {:?}", config);
 
-    /// SET THE ADRESS
+    // set the address
     let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), config.port);
     tracing::info!("Listening on {}", addr);
-    let listener = TcpListener::bind(&addr).or(Err(AppError::TcpBind))?;
+    let listener = TcpListener::bind(addr).or(Err(AppError::TcpBind))?;
 
     startup::run(listener)?
         .await
