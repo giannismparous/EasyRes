9f37909ed3ce3b1c8daefcdcfaeb71099f205ada
Dimitrios Rammos <61520191+drammos@users.noreply.github.com>
2024-10-20 14:52:43 +0300

    Update the user impl and struct for id

--- easy-res-server/src/models/user.rs
+++ easy-res-server/src/models/user.rs
@@ -1,35 +1,37 @@
 use serde::{Deserialize, Serialize};
-use uuid::Uuid;
+use chrono::NaiveDate;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct User {
-    #[serde(skip_serializing_if = "Option::is_none")] // If is none
-    #[serde(skip_deserializing)] // Skip the deserialize for Id
-    pub id: Option<Uuid>,
-
+    #[serde(skip_deserializing)]
+    pub id: String,
     pub username: String,
     pub firstname: String,
     pub lastname: String,
     pub email: String,
+    pub birthdate: NaiveDate,
     pub is_active: bool,
     pub is_admin: bool,
 }
 
 impl User {
     pub fn new(
+        id: String,
         username: String,
         firstname: String,
         lastname: String,
         email: String,
+        birthdate: NaiveDate,
         is_active: bool,
         is_admin: bool,
     ) -> User {
         User {
-            id: Some(Uuid::new_v4()),
+            id,
             username,
             firstname,
             lastname,
             email,
+            birthdate,
             is_active,
             is_admin,
         }
